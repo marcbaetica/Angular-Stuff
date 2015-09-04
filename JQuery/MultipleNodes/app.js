29882cc84a298af3ddd92d56943f071275ae0f65
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-09-03 23:13:41 -0400

    fixed files + sleectors

---
+++ JQuery/MultipleNodes/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	//notation is just like CSS selectors
+	// $("li").text("same thing");							//select DOM node
+	// $("#groceries").text("one thig selected by ID");		//select by ID
+	$(".antiDoctor").text("many apples");					//select by class
+})

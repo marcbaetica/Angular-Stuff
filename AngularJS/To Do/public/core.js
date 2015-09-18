1f3144ac471c64730ce08c56a967fd89659052e2
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-09-18 06:14:03 -0400

    angular controller

--- AngularJS/To Do/public/core.js
+++ AngularJS/To Do/public/core.js
@@ -2,12 +2,41 @@
 
 var todoApp = angular.module('todoApp', []);
 
-function mainController($scope, $http) {
-	$scope.formData = {};
+function mainController ($scope, $http) {
+	$http.formData = {};	//create an empty object (node for josh: x = {} is a object, x = [] is an array ^^)
 
-	//when landing on the page, get all todos and show them
+	//landing on the page and get all todos and show them
 	$http.get('/api/todos')
-		.success(function(data) {$scope.todo = data; console.log(data)})
-		.error(function(data) {console.log("Error: " + data)});
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(e) {
+			console.log('Error: ' + e);
+		});
 
+	//when submitting the add from, send the text to the node API
+	$scope.createTodo = function() {
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};	//clear the form so our user is ready to enter another 
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(e) {
+				console.log('Error: ' + e);
+			});
+	};
+
+	//deleting a todo after checking it
+	$scope.deleteTodo = function (id) {
+		$http.delete('/api/todos' + id)
+			.success(function (data) {
+				$scope.todos = data;
+				console.log(data);
+			})
+			.erro(function (e) {
+				console.log('Error: ' + e);
+			});
+	};
 }
